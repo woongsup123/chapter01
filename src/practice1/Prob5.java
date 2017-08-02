a9bf9f38b08de57f36479c151fc090a383f9d826
bit-user <bit-user@DESKTOP-CNKQU76>
2017-08-02 09:10:54 +0900

    change algorithm in practice1-5

--- src/practice1/Prob5.java
+++ src/practice1/Prob5.java
@@ -5,12 +5,16 @@ public class Prob5 {
 	public static void main(String[] args) {
 		for (int i = 1; i <=99; i++) {
 			int n = 0;
-			if (i >= 10) {
-				if ((i/10) % 3 == 0) { //10의 자리
+			String str = String.valueOf(i);
+			int num;
+			if (str.length() == 2) {
+				num = Character.getNumericValue(str.charAt(1));
+				if (num != 0 && num % 3 == 0) { //10의 자리
 					n++;
 				}
 			}
-			if (i % 10 != 0 && (i - i/10 * 10) % 3 == 0) { //1의 자리
+			num = Character.getNumericValue(str.charAt(0));
+			if (num != 0 && num % 3 == 0) { //1의 자리
 				n++;
 			}
 			if (n != 0) {
